2b00cbb0b5b7456f3d1b11a005a4182e78ea8943
aovsybo <matgr2n@gmail.com>
2023-07-16 22:14:42 +0300

    init

---
+++ src/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+
+from redis import asyncio as aioredis
+
+from src.auth.base_config import auth_backend, fastapi_users
+from src.auth.schemas import UserRead, UserCreate
+from src.operations.router import router as router_operation
+from src.tasks.router import router as router_tasks
+
+
+app = FastAPI(
+    title="Trading App"
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["Auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Auth"],
+)
+
+app.include_router(router_operation)
+app.include_router(router_tasks)
+
+
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url("redis://localhost")
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
